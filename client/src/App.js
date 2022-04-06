abfba26a066990d315eab84f252d9344f229ba87
PezzottiCarlo <carlo.pezzotti@student.supsi.ch>
2022-04-06 14:53:05 +0200

    feat: :sparkles: first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='section'>
+        <h1>Sezione 1</h1>
+      </div>
+      <div className='section'>
+        <h1>Sezione 2</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
